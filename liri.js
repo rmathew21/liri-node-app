49b16876ede09b74496f954511c95c82fb873f80
rob <robmathew21@gmail.com>
2019-05-03 00:01:15 -0500

    spotify added

--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
-require("dotenv").config();
+require('dotenv').config();
+
+const Spotify = require('node-spotify-api');
 
 const keys = require("./keys.js");
 
 const spotify = new Spotify(keys.spotify);
+
+spotify.search({
+    type: 'track',
+    query: 'All the Small Things',
+}, function(err, data) {
+    if (err) {
+        return console.log('Error occured: ' + err);
+    }
+    console.log(data);
+});
